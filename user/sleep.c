15c5b7cd71d0b653ebf4a82103580d226011bb7d
hwf <23302010049@m.fudan.edu.cn>
2025-09-28 21:25:18 +0800

    feat: sleep executabl accomplished.

--- user/sleep.c
+++ user/sleep.c
@@ -1,1 +1,18 @@
-please change it!
+// please change it!
+#include <user/user.h>
+
+int main(int argc, char *argv[]) {
+  int i;
+  if (argc <= 1) {
+    fprintf(2, "sleep: missing operand\nusage: sleep 1 2 3,...\n");
+    exit(1);
+  }
+
+  int t = 0;
+  for (i = 1; i < argc; i++) {
+    t += atoi(argv[i]);
+  }
+  sleep(t);
+
+  exit(0);
+}
